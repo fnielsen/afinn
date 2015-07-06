c7ec4c97181e5ba76ed5c8ac400d5dfb1eb9c789
fnielsen <faan@dtu.dk>
2015-07-06 16:53:03 +0200

    Initial version.

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+
+from setuptools import setup
+
+
+setup(
+    name='afinn',
+    version='0.1dev',
+    author='Finn Aarup Nielsen',
+    author_email='faan@dtu.dk',
+    description='AFINN Sentiment analysis',
+    license='GPL',
+    keywords='sentiment analysis',
+    url='https://github.com/fnielsen/afinn',
+    long_description='',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        ],
+    test_requires=['pytest', 'flake8', 'flake8-docstrings'],
+    )

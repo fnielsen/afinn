3569804b717e0bd4a23cc561c25283c127cd73d1
fnielsen <faan@dtu.dk>
2015-07-07 19:37:15 +0200

    Added test functions.

--- test_afinn.py
+++ test_afinn.py
@@ -4,6 +4,7 @@ import sys
 
 from afinn import Afinn
 
+
 # https://stackoverflow.com/questions/6625782
 if sys.version_info[0] == 2: 
     import codecs
@@ -23,14 +24,31 @@ def test_find_all():
     assert words == ['bad']
 
 
+def test_split():
+    afinn = Afinn()
+    words = afinn.split('Hello, World')
+    assert words == ['Hello', 'World']
+
+    words = afinn.split(u('Hell\xf8, \xc5rld'))
+    assert words == [u('Hell\xf8'), u('\xc5rld')]
+
+
 def test_score():
     afinn = Afinn()
     score = afinn.score('bad')
     assert score < 0
 
+    score = afinn.score('')
+    assert score == 0.0
 
-def test_score_language(language='en'):
-    afinn = Afinn()
+
+
+def test_score_language():
+    afinn = Afinn(language='en')
+    score = afinn.score('bad')
+    assert score < 0
+
+    afinn = Afinn('en')
     score = afinn.score('bad')
     assert score < 0
 
@@ -40,3 +58,47 @@ def test_unicode():
     score = afinn.score(u('na\xefve'))
     assert score < 0
 
+
+def test_danish():
+    afinn = Afinn(language='da')
+    score = afinn.score('bedrageri')
+    assert score < 0
+
+    score = afinn.score(u('besv\xe6r'))
+    assert score < 0
+
+
+def test_score_with_pattern():
+    afinn = Afinn(language='da')
+    score = afinn.score('ikke god')
+    assert score < 0
+
+    score = afinn.score('ikke god.')
+    assert score < 0
+
+    score = afinn.score('IKKE GOD-')
+    assert score < 0
+
+    score = afinn.score('ikke   god')
+    assert score < 0
+
+    score = afinn.score('En tv-succes sidste gang.')
+    assert score > 0
+
+    score = afinn.score('')
+    assert score == 0.0
+
+
+def test_score_with_wordlist():
+    afinn = Afinn()
+    score = afinn.score('Rather good.')
+    assert score > 0
+
+    score = afinn.score('Rather GOOD.')
+    assert score > 0
+
+
+def test_score_with_wordlist_empty():
+    afinn = Afinn()
+    score = afinn.score('')
+    assert score == 0.0

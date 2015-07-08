6f74ecc0f7121f6baab2ab7125e0d05f72501be4
fnielsen <faan@dtu.dk>
2015-07-08 18:25:21 +0200

    Added package data.

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-
+"""For packaging and installation."""
 
 from setuptools import setup
 
 
 setup(
     name='afinn',
-    version='0.1dev',
+    packages=['afinn'],
+    version='0.1.dev0',
     author='Finn Aarup Nielsen',
     author_email='faan@dtu.dk',
-    description='AFINN Sentiment analysis',
+    description='AFINN sentiment analysis',
     license='GPL',
     keywords='sentiment analysis',
     url='https://github.com/fnielsen/afinn',
+    package_data={'afinn': ['data/*.txt', 'data/LICENSE']},
     long_description='',
     classifiers=[
         'Programming Language :: Python :: 2.7',

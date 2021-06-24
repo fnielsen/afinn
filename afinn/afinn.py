ef4fffe9b11e899e43bc02dd03766e3cc42b2543
Finn Årup Nielsen <faan@dtu.dk>
2021-06-24 09:41:49 +0200

    Add Polish support

--- afinn/afinn.py
+++ afinn/afinn.py
@@ -16,6 +16,7 @@ LANGUAGE_TO_FILENAME = {
     'emoticons': 'AFINN-emoticon-8.txt',
     'fi': 'AFINN-fi-165.txt',
     'fr': 'AFINN-fr-165.txt',
+    'pl': 'AFINN-pl-165.txt',
     'sv': 'AFINN-sv-165.txt',
     'tr': 'AFINN-tr-165.txt',
     }
